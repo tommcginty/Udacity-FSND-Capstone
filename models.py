c65ee4562ba25764cf6340879ed4e7f253c8f07b
Tom McGinty <thmcginty@gmail.com>
2020-05-07 19:29:01 -0400

    Initial commit

---
+++ models.py
@@ -0,0 +1,102 @@
+import os
+from sqlalchemy import Column, String, Integer, create_engine, Date, Float
+from flask_sqlalchemy import SQLAlchemy
+import json
+from datetime import date
+from config import database_setup
+from app import db
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String,nullable=False)
+    title = db.Column(db.String)
+    release_date = db.Column(db.String())
+
+    def __repr__(self):
+        return '<Title {}>'.format(self.title)
+
+    def __init__(self, title, genre, release_date):
+        self.title = title
+        self.genre = genre
+        self.release_date = release_date
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+        'id': self.id,
+        'title' : self.title,
+        'release_date': self.release_date
+        }
+    
+
+class Actor(db.Model):
+    __tablename__ = 'actors'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200),nullable=False)
+    gender = db.Column(db.String(10))
+    birthdate = db.Column(db.String())
+
+    def __repr__(self):
+        return '<Name {}>'.format(self.name)
+
+    def __init__(self, name, gender, birthdate):
+        self.name = name
+        self.gender = gender
+        self.birthdate = birthdate
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+        'id': self.id,
+        'name' : self.name,
+        'gender': self.gender,
+        'age': self.birthdate
+        }
+
+def db_drop_and_create_all():
+    '''drops the database tables and starts fresh
+    can be used to initialize a clean database
+    '''
+    db.drop_all()
+    db.create_all()
+    db_init_records()
+
+def db_init_records():
+    '''this will initialize the database with some test records.'''
+
+    new_actor = (Actor(
+        name = 'Cory McAbee',
+        gender = 'Male',
+        birthdate = '1961/08/29'
+        ))
+
+    new_movie = (Movie(
+        title = 'The American Astronaut',
+        genre = 'Comedy',
+        release_date = '2001/01/20'
+        ))
+
+    new_actor.insert()
+    new_movie.insert()
+    db.session.commit()
