55622099814b2900b4bfcc16b4dcd14b1d775273
Tom McGinty <thmcginty@gmail.com>
2020-05-08 09:18:03 -0400

    Update models

--- app.py
+++ app.py
@@ -2,15 +2,42 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from dotenv import load_dotenv
+from models import setup_db, db_drop_and_create_all
+
+load_dotenv()
+
+RESULTS_PER_PAGE = 6
+
+def paginate_results(request, selection):
+  page = request.args.get('page', 1, type=int)
+  start = (page - 1) * RESULTS_PER_PAGE
+  end = start + RESULTS_PER_PAGE
+
+  results = [question.format() for result in selection]
+  current_results = results[start:end]
+  
+  return current_results
 
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
+  setup_db(app)
   CORS(app)
+
+  db_drop_and_create_all():
+
+  @app.route('/')
+  def welcome():
+    return 'Welcome to Central Casting'
+
+#  Movies
+#  ----------------------------------------------------------------  
 
   return app
 
-APP = create_app()
+app = create_app()
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
+    
--- models.py
+++ models.py
@@ -1,10 +1,23 @@
-import os
-from sqlalchemy import Column, String, Integer, create_engine, Date, Float
+from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
-from datetime import date
-from config import database_setup
-from app import db
+import os
+
+database_path = os.environ['DATABASE_URL']
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
 
 class Movie(db.Model):
     __tablename__ = 'movies'
@@ -80,7 +93,6 @@ def db_drop_and_create_all():
     '''
     db.drop_all()
     db.create_all()
-    db_init_records()
 
 def db_init_records():
     '''this will initialize the database with some test records.'''
