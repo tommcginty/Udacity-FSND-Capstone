c5c9a3e3b1006fa88499e760f7f91287e03cce0a
Tom McGinty <thmcginty@gmail.com>
2020-05-09 14:29:42 -0400

    Adds test for get questions

--- test_app.py
+++ test_app.py
@@ -1,1 +1,51 @@
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
 
+from app import create_app
+from models import setup_db, Movie, Actor
+
+class CastingTestCase(unittest.TestCase):
+    """This class represents the casting agenct test case"""
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = "casting_test"
+        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        setup_db(self.app, self.database_path)
+
+        # test movie
+        self.new_movie = {
+            'title': 'Test Title',
+            'genre': 'Comedy',
+            'relese_date': '2038/01/19'
+        }
+
+
+        # binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # create all tables
+            self.db.create_all()
+    
+    def tearDown(self):
+        """Executed after reach test"""
+        pass
+
+    def test_get_paginated_movies(self):
+        res = self.client().get('/movies')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data["total_movies"])
+        self.assertTrue(len(data["movies"]))
+    
+    
+
+if __name__ == "__main__":
+    unittest.main()
